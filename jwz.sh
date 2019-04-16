0717ba78825d96bf0edc230f89a082fc5211f1b3
Weizhong Jin <weizhonj@usc.edu>
2019-04-16 01:37:36 -0700

    "Update": shell update

--- jwz.sh
+++ jwz.sh
@@ -1,1 +1,18 @@
+cd pytorch-faster-rcnn/lib
+python setup.py build develop
+cd ../data
+git clone https://github.com/pdollar/coco.git
+cd coco/PythonAPI
+make
+cd ../../..
+mkdir VOCdevkit2007
+cd VOCdevkit2007
+wget https://s3.amazonaws.com/weizhongjin/VOC2012.zip
+unzip VOC2012.zip
+mv VOC2012 VOC2007
+cd VOC2007/ImageSets/Main
+rm trainval.txt
+wget https://s3.amazonaws.com/weizhongjin/txt.zip
+unzip txt.zip
+cd ../../../../..
 CUDA_VISIBLE_DEVICES=0 python trainval_net.py --dataset pascal_voc --net vgg16 --bs 12 --nw 1  --cuda
